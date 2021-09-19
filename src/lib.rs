dc9914f77ba3c7fcafc9871356d3ae66d3da2a55
James Goodall <jatgoodall@protonmail.com>
2021-09-19 10:59:32 +0100

    Finished database interaction functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-use worker::*;
 use futures::future::join_all;
+use worker::*;
 mod utils;
-
 
 fn log_request(req: &Request) {
     console_log!(
@@ -17,27 +16,59 @@ fn log_request(req: &Request) {
 pub async fn main(req: Request, env: Env) -> Result<Response> {
     log_request(&req);
     utils::set_panic_hook();
-
-
-    let index = include_str!("html/index.html");
-    let stylesheet = include_str!("html/stylesheet.css");
-    Response::from_html(index)
-}
-
-async fn get_post_content(env: Env, postid: String) -> Result<String> {
-    todo!()
+    Response::from_html("hi")
 
 }
 
-async fn get_replies(env: Env, postid: String) -> Result<Vec<(String,String)>> {
+async fn get_content(env: Env, post_id: String) -> Result<Option<String>> {
+    let prefix = get_prefix(post_id.as_str());
+    let data = env.kv("POSTS")?.get(prefix.as_str()).await?;
+    let content: Option<String>;
+    if let Some(contents) = data {
+        content = Some(contents.as_string());
+    } else {
+        content = None;
+    }
+    
+    Ok(content)
+
+}
+
+async fn post_content(env: Env, post_id: &str, contents: &str) -> Result<()> {
     let kv = env.kv("POSTS")?;
-    let keys = kv.list().execute().await?.keys;
-    let values = keys.iter()
-        .map(|key| {
-            kv.get(key.name.as_str())
-        })
-        .collect::<Vec<_>>();
-    let posts = join_all(values).await;
-    console_log!("{:#?}", posts);
-    todo!()
+    let prefix = get_prefix(post_id);
+    kv.put(prefix.as_str(), contents)?.execute().await?;
+    Ok(())
+
+}
+
+async fn get_replies(env: Env, post_id: &str) -> Result<Vec<(String, String)>> {
+    let limit = 50;
+    let prefix = get_prefix(post_id);
+
+    let keys = env
+        .kv("POSTS")?
+        .list()
+        .prefix(prefix)
+        .limit(limit)
+        .execute()
+        .await?;
+    let kv = env.kv("POSTS")?;
+    let replies = keys.keys.iter().map(|key| {
+        let key_name = key.name.as_str();
+        let body = kv.get(key_name);
+        async move { (key_name.trim_start().to_string(), body.await.unwrap().unwrap().as_string()) }
+    });
+    let test = join_all(replies).await;
+    Ok(test)
+}
+
+fn get_prefix(post_id: &str) -> String {
+    let key_length = post_id.len();
+    let zeros = " "
+        .chars()
+        .cycle()
+        .take(512 - key_length)
+        .collect::<String>();
+    format!("{}{}", zeros, post_id)
 }
