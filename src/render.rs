62f302328dbcf2b91fe395b36d24c61ea4530296
James Goodall <jatgoodall@protonmail.com>
2021-10-04 18:41:33 +0100

    added logged in/out indicator

--- src/render.rs
+++ src/render.rs
@@ -44,8 +44,8 @@ pub async fn render_page(
         .replace("<!--replies-->", replies_html.as_str());
 
     response = match user {
-        Some(user) => response.replace("<!--username-->", user.user_id.as_str()),
-        None => response.replace("<!--username-->", ""),
+        Some(user) => response.replace("<!--username-->", format!("Hi {}!", user.user_id.as_str()).as_str()),
+        None => response.replace("<!--username-->", format!("You are not logged in!").as_str()),
     };
 
     let html = match is_login_error {
